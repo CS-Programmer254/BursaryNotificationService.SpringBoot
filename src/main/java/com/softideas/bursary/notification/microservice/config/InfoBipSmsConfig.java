191bc46f831e53d856aa7a972765b66296eeb0c8
CS-Programmer254 <stanleigh3oduor@gmail.com>
2025-02-20 01:32:52 +0300

    Email Service,Sms Service,Broker Service tested and working

---
+++ src/main/java/com/softideas/bursary/notification/microservice/config/InfoBipSmsConfig.java
@@ -0,0 +1,34 @@
+//package com.softideas.bursary.notification.microservice.config;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class InfoBipSmsConfig {
+//
+//    @Value("${infobip.api.url}")
+//    private String baseUrl;
+//
+//    @Value("${infobip.api.key}")
+//    private String apiKey;
+//
+//    @Value("${infobip.api.sender.id}")
+//    private String appPhoneNumber;
+//
+//    @Bean
+//    public String getBaseUrl() {
+//        return baseUrl;
+//    }
+//
+//    public String getAppPhoneNumber() {
+//
+//        return  appPhoneNumber;
+//    }
+//
+//    public String getAPIKey() {
+//        return apiKey;
+//    }
+//
+//
+//}
